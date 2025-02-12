74b979a95b6fa430812b9793b770ade69cf833e2
indaandiny5 <2223598.inda@smkn-2sbg.sch.id>
2025-02-12 13:46:39 +0700

    Memberikan komentar pada TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,5 +1,6 @@
 <?php
 
+// task controller digunakan untuk menampilkan fungsi daftar tugas, menambahkan tugas baru
 namespace App\Http\Controllers;
 
 use App\Models\Task;
@@ -8,7 +9,9 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index() {
+    // untuk menampilkan daftar tugas
+    public function index()
+    {
         // untuk mengambil data variable yang ada di dalam folder models/task
         $data = [
             'title' => 'Home',
@@ -16,41 +19,64 @@ class TaskController extends Controller
             'lists' => TaskList::all(),
             // lists untuk mengambil semua TaskList yang ada di folder models/TaskList
             'tasks' => Task::orderBy('created_at', 'desc')->get(),
-             // orderBy desc mengurutkan dari yang terbesar ke yang terkecil
+            // orderBy desc mengurutkan dari yang terbesar ke yang terkecil
             'priorities' => Task::PRIORITIES
             // untuk mengambil nilai priorities dari const yang ada di app/models/task
         ];
-    
+
 
         return view('pages.home', $data);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $request->validate([
             'name' => 'required|max:100',
-            'list_id' => 'required'
+            'list_id' => 'required',
+            'description' => 'nullable|max:100',
+            'priority' => 'required|in:high,medium,low'
         ]);
-
+        // public function store(Request $request) digunakan untuk menyimpan data baru ke dalam basis data
+        // nullable artinya boleh dkosongkan
+        // priority digunakan untuk menambahkan data 
         Task::create([
             'name' => $request->name,
-            'list_id' => $request->list_id
+            'list_id' => $request->list_id,
+            'description' => $request->description,
+            'priority' => $request->priority
         ]);
 
 
         return redirect()->back();
     }
 
-    public function complete($id) {
+    public function complete($id)
+    {
         Task::findOrFail($id)->update([
             'is_completed' => true
         ]);
 
         return redirect()->back();
+        // untuk mengembalikan pengguna ke halaman sebelumnya setelah tugas diperbarui
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
         Task::findOrFail($id)->delete();
 
         return redirect()->back();
+        // Mengembalikan pengguna ke halaman sebelumnya setelah tugas dihapus
+    }
+
+    public function show($id)
+    {
+        $task = Task::findOrfail($id);
+        // findOrfail,untuk Mmencari tugas berdasarkan id
+        $data = [
+            'title' => 'Details',
+            'task' => $task,
+        ];
+        return view('pages.details', $data);
+        // untuk mengirim data ke tampilan (pages.details) untuk ditampilkan kepada pengguna
     }
 }
